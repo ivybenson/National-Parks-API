b5f933758c72903dfa45759df0df8111d6a5cee5
ivybenson <68450827+ivybenson@users.noreply.github.com>
2020-08-16 16:42:11 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,33 +3,30 @@
 const apiKey = "OABj953aUhLX65ddNYahUSeyUUbuRjquhvhoPFgX";
 const searchUrl = "https://developer.nps.gov/api/v1/parks";
 
-//formats the search options
-function formatSearch() {
-  console.log("FORMAT");
-}
-
 //displays up to max results for given states
 function displayResults(responseJson) {
   console.log("display results");
+  console.log(responseJson);
   $(".search-results").empty();
-  $(".results").removeClass("hidden");
-  for (let i = 0; i < responseJson.length; i++) {
-    $("#search-results").append(`<li>
-    <h3>${responseJson.items[i].fullName}</h3>
-    <p>${responseJson.items[i].description}</p>
-    <a href="${responseJson.items[i].url}">${responseJson.items[i].url}</a>
+  for (let i = 0; i < responseJson.data.length; i++) {
+    $(".search-results").append(`<li>
+    <h3>${responseJson.data[i].fullName}</h3>
+    <img src="${responseJson.data[i].images[1].url}" alt="first-gallery-image">
+    <p>${responseJson.data[i].description}</p>
+    <a href="${responseJson.data[i].url}">${responseJson.data[i].url}</a>
     </li>`);
   }
+  $(".results").removeClass("hidden");
 }
 
-//parks?stateCode=MN&api_key=OABj953aUhLX65ddNYahUSeyUUbuRjquhvhoPFgX
 
 //calls fetch function for National Parks API
 function getParks() {
   console.log("get parks");
   const searchTerm = $("#js-search-term").val();
+  const resultMax = $("#js-max-results").val();
   fetch(
-    `https://developer.nps.gov/api/v1/parks?stateCode=${searchTerm}&api_key=OABj953aUhLX65ddNYahUSeyUUbuRjquhvhoPFgX`
+    `https://developer.nps.gov/api/v1/parks?stateCode=${searchTerm}&api_key=OABj953aUhLX65ddNYahUSeyUUbuRjquhvhoPFgX&limit=${resultMax}`
   )
     .then((response) => response.json())
     .then((responseJson) => displayResults(responseJson));
@@ -41,9 +38,8 @@ function watchForm() {
     event.preventDefault();
     console.log("watchform");
     getParks();
-    //displayResults();
-    formatSearch();
   });
 }
 
 $(watchForm);
+
